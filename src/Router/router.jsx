9f9757c7a93080a793916db670786e7e207e92d7
Ramant <ramant@inqui-lab.org>
2024-07-25 15:27:08 +0530

    lp url

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -93,7 +93,10 @@ const AllRoutes = () => {
   //     <ThemeSettings />
   //   </div>
   // );
-
+  function MyComponent() {
+    window.location.href = `${process.env.REACT_APP_LANDING_PAGE_URL}`;
+    return null;
+}
   return (
     <div>
       <Routes>
@@ -102,7 +105,8 @@ const AllRoutes = () => {
             <Route path={route.path} element={route.element} key={id} />
           ))} */}
         {/* </Route> */}
-        <Route path="/" element={<Navigate to="/teacher" />} />
+
+        <Route path="/" exact={true} element={<MyComponent />} />
         <Route path="/">
           <Route path="/registration" element={<AtlPage />} />
           <Route path="/login" element={<LoginPage />} />
