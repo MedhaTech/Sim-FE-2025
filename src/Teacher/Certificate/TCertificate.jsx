c9f83a9bb9a5ba74041b0849781e9dc60eacf1d0
shaikarshiya@inqui-lab.org <shaikarshiya@inqui-lab.org>
2024-11-18 15:09:50 +0530

    New

--- src/Teacher/Certificate/TCertificate.jsx
+++ src/Teacher/Certificate/TCertificate.jsx
@@ -120,7 +120,46 @@ class TCertificate extends React.Component {
                 </div> 
             </div>
     ) : (
-        <h2 className='text-center' style={{color:"blue"}}>Certificates Are Coming Soon ...</h2>
+        <>
+        <h2 className='text-center' style={{ color: "blue" }}>Tamil Nadu </h2><div className="container-fluid bg-white">
+                            <div className="row">
+                                <div style={{ position: 'relative' }}>
+                                    <span
+                                        className="text-capitalize"
+                                        style={{
+                                            position: 'absolute',
+                                            top: '31rem',
+                                            left: '10rem',
+                                            fontSize: '1.2rem',
+                                            fontFamily: 'Times New Roman'
+                                        }}
+                                    >
+                                        {this.props.title}{' '}
+                                        {this.props.full_name}
+                                    </span>
+                                    <span
+                                        className="text-capitalize"
+                                        style={{
+                                            position: 'absolute',
+                                            top: '34rem',
+                                            left: '11rem',
+                                            fontSize: '1.2rem',
+                                            fontFamily: 'Times New Roman'
+                                        }}
+                                    >
+                                        {this.props.organization_name}
+                                    </span>
+                                    <img
+                                        src={TeacherCertificate}
+                                        alt="certificate"
+                                        style={{
+                                            width: '1000px',
+                                            height: '1110px',
+                                            // border: '1px solid #ccc' 
+                                        }} />
+                                </div>
+                            </div>
+                        </div></>
     )}
             </div>  
         ); 
