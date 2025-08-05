7db10d454bb3865fe37e4c4a91c09277c1a75005
Ramant <ramant@medhatech.in>
2025-08-05 10:04:57 +0530

    key change for mal language

--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@ i18n.use(LanguageDetector)
     .use(initReactI18next)
     .use(HttpApi)
     .init({
-        supportedLngs: ['en', 'tn', 'te', 'ka', 'hi','mal'],
+        supportedLngs: ['en', 'tn', 'te', 'ka', 'hi','ml'],
         fallbackLng: 'en',
         detection: {
             order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
