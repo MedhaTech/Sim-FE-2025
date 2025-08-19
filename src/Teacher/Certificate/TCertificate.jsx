9f5c20e0f054d7730f01108f74ebebbf04a4a326
shaikarshiya@inqui-lab.org <shaikarshiya@inqui-lab.org>
2025-08-19 15:57:25 +0530

    new

--- src/Teacher/Certificate/TCertificate.jsx
+++ src/Teacher/Certificate/TCertificate.jsx
@@ -1,39 +1,40 @@
 /* eslint-disable indent */
 
-import React from 'react'; 
+import React from "react";
 import TeacherCertificate from "../../assets/img/Certificates/SIM3Teacher1.png";
-import TnTeacherCertificate from "../../assets/img/Certificates/SIM3Teacher1.png";  
+import TnTeacherCertificate from "../../assets/img/Certificates/SIM3Teacher1.png";
 
 import { getCurrentUser } from "../../helpers/Utils";
 
-class TCertificate extends React.Component { 
-    constructor(props) { 
-        super(props); 
-        this.state = {}; 
-        const currentUser = getCurrentUser("current_user");
-        this.stateSpecific = currentUser?.data[0]?.state;
-    } 
-    render() { 
-        return (
-            <div>
-               {this.stateSpecific !== "Tamil Nadu" ? ( 
-            <div style={{ width: '100%', margin: 0, padding: 0 , overflow: 'hidden',}}> 
-                <div className="row"> 
-                    <div > 
-                        <span 
-                            className="text-capitalize" 
-                            style={{ 
-                                position: 'absolute', 
-                                top: '30rem', 
-                                left: '10rem',
-                                fontSize: '1.5rem',
-                                fontFamily: 'Times New Roman' 
-                            }} 
-                        > 
-                            {this.props.title}{' '} 
-                            {this.props.full_name} 
-                        </span> 
-                        {/* <span 
+class TCertificate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    const currentUser = getCurrentUser("current_user");
+    this.stateSpecific = currentUser?.data[0]?.state;
+  }
+  render() {
+    return (
+      <div>
+        {this.stateSpecific !== "Tamil Nadu" ? (
+          <div
+            style={{ width: "100%", margin: 0, padding: 0, overflow: "hidden" }}
+          >
+            <div className="row">
+              <div>
+                <span
+                  className="text-capitalize"
+                  style={{
+                    position: "absolute",
+                    top: "30rem",
+                    left: "10rem",
+                    fontSize: "1rem",
+                    fontFamily: "Times New Roman",
+                  }}
+                >
+                  {this.props.title} {this.props.full_name}
+                </span>
+                {/* <span 
                             className="text-capitalize" 
                             style={{ 
                                 position: 'absolute', 
@@ -45,37 +46,43 @@ class TCertificate extends React.Component {
                         > 
                             {this.props.organization_name} 
                         </span>  */}
-                       
-                        <img 
-                            src={TeacherCertificate} 
-                            alt="certificate" 
-                            style={{ 
-                                width: '1000px',  
-                                height: '1110px',  
-                            }} 
-                        /> 
-                    </div> 
-                </div> 
+
+                <img
+                  src={TeacherCertificate}
+                  alt="certificate"
+                  style={{
+                    width: "1000px",
+                    height: "1110px",
+                  }}
+                />  
+              </div>
             </div>
-    ) : (
-        <>
-        <div  style={{ width: '100%', margin: 0, padding: 0 , overflow: 'hidden',}}>
-                            <div className="row">
-                                <div >
-                                    <span
-                                        className="text-capitalize"
-                                        style={{
-                                            position: 'absolute',
-                                            top: '30rem',
-                                            left: '14rem',
-                                            fontSize: '1.5rem',
-                                            fontFamily: 'Times New Roman'
-                                        }}
-                                    >
-                                        {this.props.title}{' '}
-                                        {this.props.full_name}
-                                    </span>
-                                    {/* <span
+          </div>
+        ) : (
+          <>
+            <div
+              style={{
+                width: "100%",
+                margin: 0,
+                padding: 0,
+                overflow: "hidden",
+              }}
+            >
+              <div className="row">
+                <div>
+                  <span
+                    className="text-capitalize"
+                    style={{
+                      position: "absolute",
+                      top: "30rem",
+                      left: "14rem",
+                      fontSize: "1rem",
+                      fontFamily: "Times New Roman",
+                    }}
+                  >
+                    {this.props.title} {this.props.full_name}
+                  </span>
+                  {/* <span
                                         className="text-capitalize"
                                         style={{
                                             position: 'absolute',
@@ -87,7 +94,7 @@ class TCertificate extends React.Component {
                                     >
                                         {this.props.organization_name}
                                     </span> */}
-                                    {/* <span 
+                  {/* <span 
                             className="text-capitalize" 
                             style={{ 
                                 position: 'absolute', 
@@ -100,20 +107,22 @@ class TCertificate extends React.Component {
                         > 
                             {new Date().toLocaleDateString('en-GB')} 
                         </span> */}
-                                    <img
-                                        src={TnTeacherCertificate}
-                                        alt="certificate"
-                                        style={{
-                                            width: '1000px',
-                                            height: '1110px',
-                                        }} />
-                                </div>
-                            </div>
-                        </div></>
-    )}
-            </div>  
-        ); 
-    } 
-} 
+                  <img
+                    src={TnTeacherCertificate}
+                    alt="certificate"
+                    style={{
+                      width: "1000px",
+                      height: "1110px",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default TCertificate;
